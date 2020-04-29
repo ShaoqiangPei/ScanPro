19df79a678217383074da2000bec9f1787248be7
Pain <769936726@qq.com>
2020-04-29 10:37:16 +0800

    no message

---
+++ scan/zxing/src/main/java/com/zxing/activity/Damo.java
@@ -0,0 +1,26 @@
+package com.zxing.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.zxing.R;
+import com.zxing.utils.ScanUtil;
+
+/**
+ * Title:
+ * description:
+ * autor:pei
+ * created on 2020/4/29
+ */
+public class Damo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capture);
+
+        ScanUtil.i("========我是demo log======");
+    }
+}
